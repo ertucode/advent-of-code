4841f21bcf57ff6ab09615b00a23969c5a2ee201
Cavit Ertuğrul Sırt <ertugrul.sirt@sipay.com.tr>
2023-06-05 16:23:13 +0300

    day1 - kotlin

---
+++ src/y2015/day1.kts
@@ -0,0 +1,23 @@
+import java.io.File
+
+fun readInput(): String {
+    return File("./day1-input.txt").readText()
+}
+
+fun q1(): Int = readInput().asIterable().fold(0) {acc, c -> acc + if (c == '(') {1} else {-1}}
+
+
+println("Day1-Q1: ${q1()}")
+
+fun q2(): Int {
+    val text = readInput()
+    var floor = 0
+    for (i in 0 until text.length) {
+        floor += if (text[i] == '(') {1} else {-1}
+        if (floor == -1) return i + 1
+    }
+
+    return -1
+}
+
+println("Day1-Q2: ${q2()}")
