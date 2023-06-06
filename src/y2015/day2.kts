9163f4a52e58e472e14ee45497125bc57a689079
Cavit Ertuğrul Sırt <ertugrul.sirt@sipay.com.tr>
2023-06-06 09:33:22 +0300

    day2q2 kotlin

--- src/y2015/day2.kts
+++ src/y2015/day2.kts
@@ -8,6 +8,11 @@ data class Prism(val l: Int, val w: Int, val h: Int) {
         return 2 * this.l * this.w + 2 * this.w * this.h + 2 * this.h * this.l + smallest.first * smallest.second
     }
 
+    fun q2Area(): Int {
+        val smallest = smallestTwo()
+        return 2 * smallest.first + 2 * smallest.second + this.l *  this.w* this.h
+    }
+
     fun smallestTwo(): Pair<Int, Int> {
         var arr = intArrayOf(this.l, this.w, this.h)
         arr.sort()
@@ -27,3 +32,16 @@ fun q1(): Int {
 }
 
 println("Day2 - Q1: ${q1()}")
+
+fun q2(): Int {
+    val file = File("day2-input.txt")
+    var res: Int = 0
+    file.useLines{lines -> lines.forEach {
+        val splits = it.split("x").map { it.toInt() }
+        val prism = Prism(splits[0], splits[1], splits[2])
+        res += prism.q2Area()
+    }}
+    return res
+}
+
+println("Day2 - Q2: ${q2()}")
